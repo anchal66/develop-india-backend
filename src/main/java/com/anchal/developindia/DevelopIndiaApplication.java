be1167ec5d645bad52f52f7e7499bed3025645fe
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-06 09:43:16 +0530

    Initial commit

---
+++ src/main/java/com/anchal/developindia/DevelopIndiaApplication.java
@@ -0,0 +1,13 @@
+package com.anchal.developindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopIndiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopIndiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anchal/developindia/DevelopIndiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anchal.developindia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopIndiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
