0e1193bc72caac6ada9d704b1768d3f924b11da6
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-16 23:41:38 +0530

    Adding Auth small chnges

--- src/main/java/com/anchal/developindia/dao/AuthRepository.java
+++ src/main/java/com/anchal/developindia/dao/AuthRepository.java
@@ -2,6 +2,8 @@ package com.anchal.developindia.dao;
 
 import com.anchal.developindia.entity.Auth;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface AuthRepository extends JpaRepository<Auth, Long> {
 }
